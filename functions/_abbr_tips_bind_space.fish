421ede5ace7085ee66abcff45d890a5824739e7e
Gazorby <gazorby@pm.me>
2020-04-13 15:12:11 +0200

    🐛 fix: check for abbr status in on whitespace input

--- functions/_abbr_tips_bind_space.fish
+++ functions/_abbr_tips_bind_space.fish
@@ -1,9 +1,11 @@
 function _abbr_tips_bind_space
     commandline -i " "
-    if abbr -q (string trim (commandline))
-        set -g _abbr_tips_is_abbr 1
-    else
-        set -g _abbr_tips_is_abbr 0
+    if test $_abbr_tips_is_abbr != 1
+        if abbr -q (string trim (commandline))
+            set -g _abbr_tips_is_abbr 1
+        else
+            set -g _abbr_tips_is_abbr 0
+        end
     end
     commandline -f 'expand-abbr'
 end
