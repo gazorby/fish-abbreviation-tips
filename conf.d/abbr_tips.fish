d6790e11e9b44fca74c175289b70f173e15b42b6
Gazorby <gazorby@pm.me>
2020-05-11 23:33:53 +0200

    ✨ feat: customize regexes

--- conf.d/abbr_tips.fish
+++ conf.d/abbr_tips.fish
@@ -6,6 +6,16 @@ set -g _abbr_tips_used 0
 if ! set -q ABBR_TIPS_PROMPT; set -gx ABBR_TIPS_PROMPT "\n💡 \e[1m{{ .abbr }}\e[0m => {{ .cmd }}"; end
 if ! set -q ABBR_TIPS_AUTO_UPDATE; set -gx ABBR_TIPS_AUTO_UPDATE 'background'; end
 
+# Regexes used to find abbreviation inside command
+# Only the first matching group will be tested as an abbr
+if not set -q ABBR_TIPS_REGEXES
+    set -gx ABBR_TIPS_REGEXES
+    set -a ABBR_TIPS_REGEXES '(^(\w+\s+)+(-{1,2})\w+)(\s\S+)'
+    set -a ABBR_TIPS_REGEXES '(^( ?\w+){3}).*'
+    set -a ABBR_TIPS_REGEXES '(^( ?\w+){2}).*'
+    set -a ABBR_TIPS_REGEXES '(^( ?\w+){1}).*'
+end
+
 function _abbr_tips --on-event fish_postexec -d "Abbreviation reminder for the current command"
     set -l command (string split ' ' "$argv")
     set -l cmd (string replace -r -a '\\s+' ' ' "$argv" )
@@ -39,21 +49,21 @@ function _abbr_tips --on-event fish_postexec -d "Abbreviation reminder for the c
        return
     end
 
-    # Test the command against multiple regexes :
-    #  - First test the command as is
-    #  - Then try with arguments removed      (ex : git commit -m "mucommit"  => git commit -m)
-    #  - Then try with the first three words  (ex : docker volume rm myvolume => docker volume rm)
-    #  - Then try with the first two words    (ex : docker volume rm myvolume => docker volume)
-    #  - Finally try with the first word      (ex : docker volume rm myvolume => docker)
-    if set -l abb (contains -i -- "$cmd" $_ABBR_TIPS_VALUES)
-       or set -l abb (contains -i -- (string replace -r -a '((-{1,2})\\w+)(\\s\\S+)' '$1' "$cmd") $_ABBR_TIPS_VALUES)
-       or set -l abb (contains -i -- (string replace -r -a '(^( ?\\w+){3}).*' '$1' "$cmd") $_ABBR_TIPS_VALUES)
-       or set -l abb (contains -i -- (string replace -r -a '(^( ?\\w+){2}).*' '$1' "$cmd") $_ABBR_TIPS_VALUES)
-       or set -l abb (contains -i -- (string replace -r -a '(^( ?\\w+){1}).*' '$1' "$cmd") $_ABBR_TIPS_VALUES)
+    set -l abb
+    if not set abb (contains -i -- "$cmd" $_ABBR_TIPS_VALUES)
+        for r in $ABBR_TIPS_REGEXES
+            if set abb (contains -i -- (string replace -r -a -- "$r" '$1' "$cmd") $_ABBR_TIPS_VALUES)
+                break
+            end
+        end
+    end
+
+    if test -n "$abb"
         echo -e (string replace -a '{{ .cmd }}' "$_ABBR_TIPS_VALUES[$abb]" \
                 (string replace -a '{{ .abbr }}' "$_ABBR_TIPS_KEYS[$abb]" "$ABBR_TIPS_PROMPT"))
-        return
     end
+
+    return
 end
 
 function __abbr_tips_uninstall --on-event abbr_tips_uninstall
@@ -67,6 +77,7 @@ function __abbr_tips_uninstall --on-event abbr_tips_uninstall
     set --erase ABBR_TIPS_PROMPT
     set --erase ABBR_TIPS_AUTO_UPDATE
     set --erase ABBR_TIPS_ALIAS_WHITELIST
+    set --erase ABBR_TIPS_REGEXES
     functions --erase _abbr_tips_init
     functions --erase _abbr_tips_bind_newline
     functions --erase _abbr_tips_bind_space
